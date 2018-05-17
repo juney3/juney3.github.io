6df250082581ffa156cf64a7c3d1bbdc7d8409eb
juneyoshii <juney37@gmail.com>
2018-05-17 11:22:10 -0700

    redid html containers for materialize

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
 $( document ).ready(function() {
   console.log("ready to go!");
 });
-
-header {
-  background-color: yellow;
-}
-
-main {
-  background-color: lightgreen;
-}
-
-footer {
-  background-color: hotpink;
-}
